a7338f82c79248e4379e903c00b519dccc5d8b18
ニンテン <awsomestronhero@gmail.com>
2025-07-27 13:45:50 +0100

    Ball_Drop Script has been added to Pachinko branch

---
+++ Assets/Scripts/PACHINKO LOGIC/Ball_Drop.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Drop : MonoBehaviour
+{
+    float speed = 2f;
+    public Transform DropPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
