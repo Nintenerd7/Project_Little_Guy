f767ecc78db45ddbf999555caa03ba63855f0bc0
Back2Zero <74252688+Uber-blitz@users.noreply.github.com>
2025-07-27 01:42:30 +0100

    Fixed offline calculations

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -34,14 +34,8 @@ public class Game : MonoBehaviour
         #region Pet hunger while offline
         if (PlayerPrefs.HasKey("TimeClosed"))
         {
-            string timeSaved = PlayerPrefs.GetString("TimeClosed");
-            DateTime timeClosed = DateTime.Parse(timeSaved);
-            Debug.Log("Parsed Time: " + timeClosed);
-            TimeSpan timeChange = DateTime.Now - timeClosed;
-            petHunger -= (float)timeChange.TotalHours;
-
-            OfflineCalculations(petHunger);
-            OfflineCalculations(petHappiness);
+            petHunger = OfflineCalculations(petHunger);
+            petHappiness = OfflineCalculations(petHappiness);
         }
 
         Debug.Log("Pet Hunger: " + petHunger);
@@ -54,10 +48,10 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-    void OnApplicationQuit()
+    public void OnApplicationQuit()
     {
         PlayerPrefs.SetString("TimeClosed", DateTime.Now.ToString());
         PlayerPrefs.SetFloat("PetHunger", petHunger);
@@ -65,12 +59,39 @@ public class Game : MonoBehaviour
         PlayerPrefs.Save();
     }
 
-    void OfflineCalculations(float input)
+    float OfflineCalculations(float input)
     {
-            string timeSaved = PlayerPrefs.GetString("TimeClosed");
-            DateTime timeClosed = DateTime.Parse(timeSaved);
-            Debug.Log("Parsed Time: " + timeClosed);
-            TimeSpan timeChange = DateTime.Now - timeClosed;
-            input -= (float)timeChange.TotalHours;
+        Debug.Log("Offline Calculations is running");
+        string timeSaved = PlayerPrefs.GetString("TimeClosed");
+        DateTime timeClosed = DateTime.Parse(timeSaved);
+        Debug.Log("Parsed Time: " + timeClosed);
+        TimeSpan timeChange = DateTime.Now - timeClosed;
+        return input -= (float)timeChange.TotalHours;
+    }
+
+    public void FoodButton()
+    {
+        petHunger += 0.25f;
+        petHappiness += 0.1f;
+        if (petHunger <= 0)
+        {
+            petHunger = 0;
+        }
+        else if (petHunger >= 1)
+        {
+            petHunger = 1;
+        }
+        if (petHappiness <= 0)
+        {
+            petHappiness = 0;
+        }
+        else if (petHappiness >= 1)
+        {
+            petHappiness = 1;
+        }
+        Debug.Log("Pet Hunger: " + petHunger);
+        Debug.Log("Pet Happiness: " + petHappiness);
+        petHappinessTxt.text = "Pet Mood: " + (Mathf.Round(petHappiness * 100)).ToString() + "%";
+        petHungerTxt.text = "Pet Hunger: " + (Mathf.Round(petHunger * 100)).ToString() + "%";
     }
 }
