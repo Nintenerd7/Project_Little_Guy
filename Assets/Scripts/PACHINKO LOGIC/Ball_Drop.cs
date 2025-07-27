e937d3b9c1611907c03bd2f00e7179e181563004
ニンテン <awsomestronhero@gmail.com>
2025-07-27 16:58:14 +0100

    Ball drop logic: 30%

--- Assets/Scripts/PACHINKO LOGIC/Ball_Drop.cs
+++ Assets/Scripts/PACHINKO LOGIC/Ball_Drop.cs
@@ -1,21 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Ball_Drop : MonoBehaviour
 {
-    float speed = 2f;
-    public Transform DropPos;
+    
+    float BallDirection;
+    public Rigidbody2D BallForceBody;//adds force to the ball
+    public Transform DropPos;//position of where the ball will drop
+    public GameObject Ball;//Ball Prefab
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        BallForceBody = Ball.GetComponent<Rigidbody2D>();//Rigidbody component is obtained from ball prefab
+        Direction_Dice();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))//Using mouse for now in playtesting
+        {
+            Instantiate(Ball, DropPos.position, DropPos.rotation);//Ball Spawns
+
+            //Gravity
+            Physics2D.gravity = new Vector2(BallDirection, -5);
+        }//END OF IF INPUT STATEMENT 
     }
+
+    public void Direction_Dice()
+    {
+        int randomizer = Random.Range(1, 3);
+        Debug.Log(randomizer.ToString());
+        switch (randomizer)
+        {
+            case 1:
+                BallDirection = -1;
+                break;
+            case 2:
+                BallDirection = 1;
+                break;
+            case 3:
+                BallDirection = 0;
+                break;
+        }
+    }
+
 
 }
