6447dbe9766665df4786cad8edce30958086f55c
Back2Zero <74252688+Uber-blitz@users.noreply.github.com>
2025-08-01 15:41:29 +0100

    added a silly little map for pachinko

--- Assets/Scripts/PACHINKO LOGIC/Map_Randomizer.cs
+++ Assets/Scripts/PACHINKO LOGIC/Map_Randomizer.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class Map_Randomizer : MonoBehaviour
 {
-    public GameObject[] MapIndex = new GameObject[2];//contains two of the pachinko maps
+    public GameObject[] MapIndex = new GameObject[3];//contains two of the pachinko maps
 
     // Update is called once per frame
     void Start()
     {
-        int Randomizer = Random.Range(0, 5);  //randomizes maps
+        int Randomizer = Random.Range(0, 7);  //randomizes maps
 
         if(Randomizer <= 2)
         {
+            MapIndex[2].SetActive(false);
             MapIndex[1].SetActive(false);
             MapIndex[0].SetActive(true);
         }
-        else if(Randomizer > 2 || Randomizer == 5)
+        else if(Randomizer > 2 && Randomizer <= 4)
         {
+            MapIndex[2].SetActive(false);
             MapIndex[0].SetActive(false);
             MapIndex[1].SetActive(true);
+        }
+        else if(Randomizer > 4)
+        {
+            MapIndex[2].SetActive(true);
+            MapIndex[0].SetActive(false);
+            MapIndex[1].SetActive(false);
         }
         
 
